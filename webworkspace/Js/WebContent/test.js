95c6b1d08f960a516ef518cbf15ef8ce8aeed69a
hsm <skosko00@naver.com>
2018-05-03 10:28:29 +0900

    HTML

---
+++ webworkspace/Js/WebContent/test.js
@@ -0,0 +1,9 @@
+
+    window.onload = function(){
+        document.getElementById("btn1").onclick = function() {
+            alert("첫번째 클릭");
+        }
+        document.getElementById("btn2").onclick = function() {
+            alert("두번째 클릭");
+        }
+    }
