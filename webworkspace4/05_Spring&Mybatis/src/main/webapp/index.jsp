8ca7995ab6fdd7514a1ff49741e9ef45f6f315e7
hsm <skosko00@naver.com>
2018-07-18 09:25:29 +0900

    --

--- webworkspace4/05_Spring&Mybatis/src/main/webapp/index.jsp
+++ webworkspace4/05_Spring&Mybatis/src/main/webapp/index.jsp
@@ -6,8 +6,15 @@
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 <title>메인페이지</title>
+<!-- 합쳐지고 최소화된 최신 CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css">
+<!-- 부가적인 테마 -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap-theme.min.css">
+<!-- 합쳐지고 최소화된 최신 자바스크립트 -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js"></script>
 </head>
 <body>
+<div style="padding:5px;">
 <c:choose>
 <c:when test="${sessionScope.member!=null}">
 <h3>[${sessionScope.member.userName }]님 환영합니다.</h3>
@@ -20,13 +27,14 @@
 </c:when>
 <c:otherwise>
 <form action="/login.do" method="post">
-ID : <input type="text" name="userId"><br>
-PW : <input type="password" name="userPw"><br>
-<input type="submit" value="로그인">
-<a href="/enrollView.do">회원가입</a><br>
+ID : <input type="text" name="userId" class="form-control" placeholder="아이디"><br>
+PW : <input type="password" name="userPw" class="form-control"  placeholder="비밀번호"><br>
+<input type="submit" value="로그인" class="btn btn-default">
+<a href="/enrollView.do" class="btn btn-primary">회원가입</a><br>
 </form>
 </c:otherwise>
 
 </c:choose>
+</div>
 </body>
 </html>
---
+++ webworkspace4/android_Web/src/controller/Test.java
@@ -0,0 +1,55 @@
+package controller;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import test.PaPaGo;
+
+/**
+ * Servlet implementation class Test
+ */
+@WebServlet("/Test")
+public class Test extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Test() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("파파고 호출");
+		request.setCharacterEncoding("utf-8");
+		String data = request.getParameter("data");
+		
+		System.out.println("사용자 데이터 : " + data);
+		
+		PaPaGo ppg = new PaPaGo();
+		
+		String json = ppg.testPaPaGo(data);
+		
+		response.setContentType("text/html; charset=utf-8");
+		response.getWriter().println(json);
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
